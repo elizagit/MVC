fbf820cbda1bbf194b7bfa3f885e4332875098ad
elizabeth <elibou78@hotmail.com>
2015-05-03 19:14:37 +0100

    Basic Web Api Added & Controllers & Views Linked

---
+++ HouseHotelv2/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+
+
+namespace HouseHotelv2
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            // Manually installed WebAPI 2.2 after making an MVC project.
+            GlobalConfiguration.Configure(HouseHotelv2.App_Start.WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
